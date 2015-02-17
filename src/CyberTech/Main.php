5bbbd34409db1045de8adc3c73544d0d4cc5b457
yungtechboy1 <yungtechboy1@gmail.com>
2015-02-16 22:17:18 -0600

    Update

--- src/CyberTech/Main.php
+++ src/CyberTech/Main.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * Encryption - Decryption (v0.0.0.1) by CyberTech++
+ * HungerPE (v0.0.0.1) by CyberTech++
  * Developer: CyberTech++ (Yungtechboy1)
  * Website: http://www.cybertechpp.com
  * Date: 2/16/15 9:04 PM (CST)
@@ -10,28 +10,43 @@
 
 namespace CyberTech;
 
+use pocketmine\Player;
+use pocketmine\Server;
 use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerEvent;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\event\entity\EntityDeathEvent;
+use pocketmine\event\entity\EntityLevelChangeEvent;
+use pocketmine\level\Position;
+use pocketmine\math\Vector3;
+use pocketmine\permission\Permission;
+use pocketmine\permission\PermissionAttachment;
 use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+use pocketmine\utils\TextFormat;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
+use pocketmine\event\player\PlayerRespawnEvent;
+use pocketmine\item\Item;
+use pocketmine\inventory\BaseInventory;
+use pocketmine\inventory\Inventory;
+use CyberTech\StartQueingTimer;
 
 class Main extends PluginBase implements Listener{
     
     public function onEnable() {
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        //Start 10 Min Timer to Start Game If No Game is In Progress and The Minimum People is Meet.
         return true;
     }
     
     public function onCommand(CommandSender $sender, Command $command, $label, array $args){    
     $battels = $this->battels;
     switch($command->getName()){
-        case "encrypt":
-            if (isset($args[0]) && isset($args[1])){
-                $this->Encrypt($args[0], $args[1]);
-            }
-            return true;
-        case "decrypt":
-            if (isset($args[0]) && isset($args[1])){
+        case "hg":
+            if ($args[0] == "join"){
                 $this->Encrypt($args[0], $args[1]);
             }
             return true;
@@ -43,30 +58,16 @@ class Main extends PluginBase implements Listener{
     }
     }
     
-public function Encrypt($Decrypt , $passphrase){
-        $filen = explode("/", $Decrypt);
-        $filename = $filen[((count($filen)*1)-1)];
-        //$passphrase = 'My secret';
-
-        /* Turn a human readable passphrase
-        * into a reproducable iv/key pair
-        */
-        $iv = substr(md5('iv'.$passphrase, true), 0, 8);
-        $key = substr(md5('pass1'.$passphrase, true) . 
-                   md5('pass2'.$passphrase, true), 0, 24);
-        $opts = array('iv'=>$iv, 'key'=>$key);
-
-        $fp = fopen($filename.'.enc', 'wb');
-        stream_filter_append($fp, 'mcrypt.tripledes', STREAM_FILTER_WRITE, $opts);
-        $wp = fopen('$e', 'rwb');
-        $result = fread($wp,  5000);
-        fwrite($fp, $result);
-        fclose($fp);
-        echo $result;
-        
+    
+    /* HungerPE Player Join */
+    private $queing = array();
+    public function HungerPEJoin(Player $player){
+        $playern = $player->getName();
+        array_push($this->queing, $playern);
+        //Check HungerPE for Start
         return True;
 
-}
+    }
 
 public function Decrypt($Decrypt , $passphrase){
     //$Decrypt = array("plugins/Nett/src/CyberTech/DailyCheck.php");
